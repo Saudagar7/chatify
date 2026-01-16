3c836010d17940df936e735d8565f1f357395f89
Saudagar7 <saudagargudle04@gmail.com>
2026-01-16 12:19:25 +0530

    signup implemented on the frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,13 +1,44 @@
 import {create} from "zustand";
+import {axiosInstance} from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-    authUser: {name: "john", _id: 123, age: 25},
-    isLoggedIn: false,
-    isLoading: false,
+    authUser: null,
+    isCheckingAuth: true,
+    isSigningUp: false,
 
-    login: () => {
-        console.log("We just logged in");
-        set({ isLoggedIn: true, isLoading: true });
-
+    checkAuth : async () => {
+        set({ isCheckingAuth: true });
+        try {
+            const res = await axiosInstance.get('/auth/check');
+            set({ authUser: res.data });
+        } catch (error) {
+            const status = error?.response?.status;
+            if (status && status !== 401) {
+                toast.error('Unable to verify session');
+            }
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
     },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post('/auth/signup', data);
+            set({ authUser: res.data});
+
+            toast.success("Account created successfully!");
+
+        } catch (error) {
+            toast.error(error.response.data.message);
+
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+        
+
+    
 }));
